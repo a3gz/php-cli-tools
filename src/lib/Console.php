6cfbf5993f148a1cfdc4ec10a99a48d3640e8617
Alejandro Arbiza <alejandro@roetal.com>
2024-05-13 17:11:41 -0400

    Add a console logger
    	new file:   src/lib/Console.php

---
+++ src/lib/Console.php
@@ -0,0 +1,33 @@
+<?php
+// https://misc.flogisoft.com/bash/tip_colors_and_formatting
+class Console {
+  static public function danger() {
+    $args = func_get_args();
+    if (count($args)) {
+      foreach ($args as $str) {
+        echo "\033[91m{$str}\033[0m ";
+      }
+    }
+    echo "\n";
+  }
+
+  static public function log() {
+    $args = func_get_args();
+    if (count($args)) {
+      foreach ($args as $str) {
+        echo "{$str} ";
+      }
+    }
+    echo "\n";
+  }
+
+  static public function warning() {
+    $args = func_get_args();
+    if (count($args)) {
+      foreach ($args as $str) {
+        echo "\033[93m{$str}\033[0m ";
+      }
+    }
+    echo "\n";
+  }
+}
