fecac3bd87b3d11df6a54b1093431832023840e6
Alejandro Arbiza <alejandro@roetal.com>
2024-05-13 17:10:56 -0400

    WIP Add a configuration manager
    	new file:   src/lib/Config.php

---
+++ src/lib/Config.php
@@ -0,0 +1,18 @@
+<?php
+namespace Config {
+  /**
+   * @var array
+   */
+  protected $data;
+
+  public function __construct($argv) {
+    array_shift($argv);
+  
+    $configFileName = $argv[0];
+    if (!is_readable($configFileName)) {
+      throw new \Exception("Configuration file in unreachable: {$configFileName}");
+    }
+  
+    $this->data = json_decode(file_get_contents($configFileName));
+  }
+}
