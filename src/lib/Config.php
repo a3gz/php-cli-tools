d7e389acc489e1f677f4f0151fad380de497c9ac
Alejandro Arbiza <alejandro@roetal.com>
2024-05-16 08:38:51 -0400

    Allow to run only a list of one or more specific task groups
    	modified:   bin/cli-run
    	modified:   composer.json
    	modified:   src/lib/Config.php
    	modified:   test/php-cli-tools.config.json

--- src/lib/Config.php
+++ src/lib/Config.php
@@ -7,6 +7,11 @@ class Config {
    */
   protected $data;
 
+  /**
+   * @var array
+   */
+  protected $tasks;
+
   public function __construct($argv) {
     array_shift($argv);
   
@@ -14,11 +19,38 @@ class Config {
     if (!is_readable($configFileName)) {
       throw new \Exception("Configuration file in unreachable: {$configFileName}");
     }
-  
     $this->data = json_decode(file_get_contents($configFileName), true);
+
+    $this->tasks = [];
+    if (count($argv) > 1) {
+      $tasks = explode(',', $argv[1]);
+      foreach ($tasks as $task) {
+        $task = trim($task);
+        if (!empty($task)) {
+          $this->tasks[] = $task;
+        }
+      }
+    }
   }
 
-  public function get() {
+  /**
+   * @return array
+   */
+  public function getData() {
     return $this->data;
+  }
+
+  /**
+   * @return array
+   */
+  public function getTasks() {
+    return $this->tasks;
+  }
+
+  /**
+   * @return bool
+   */
+  public function hasSpecificTasks() {
+    return count($this->tasks) > 0;
   }
 }
